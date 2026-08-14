0cd5b91c2fe96c52fbf56015cb02d5f567c10b2b
Aidan Bell <aidan.bell@cluep.com>
2026-08-14 09:45:43 -0400

    chore: tighten eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,11 @@
-import eslint from '@eslint/js';
-import prettier from 'eslint-config-prettier';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import eslint from "@eslint/js";
+import prettier from "eslint-config-prettier";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   {
-    ignores: ['**/dist/**', '**/coverage/**', '**/node_modules/**', '.changeset/**'],
+    ignores: ["**/dist/**", "**/coverage/**", "**/node_modules/**", ".changeset/**"],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
@@ -18,10 +18,14 @@ export default tseslint.config(
       },
     },
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-unused-vars': [
-        'error',
-        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        { prefer: "type-imports", fixStyle: "separate-type-imports" },
       ],
     },
   },
